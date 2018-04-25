262906a5fcbd4f9b2ab624fc2cf78c6fa1c55291
Rob Bontekoe <rbontekoe@appligate.nl>
2018-04-25 12:23:59 +0200

    UnitTest model.domain update

--- src/test/scala/model/domain/Aggregates.scala
+++ src/test/scala/model/domain/Aggregates.scala
@@ -9,6 +9,9 @@ object Aggregates {
 
   val apartment = Apartment(ApartmentId(room.apartmentId.value), person.personId, ApartmentName("Vesuvius"))
 
+  val emptyRoom = EmptyRoom(SensorImage(room.sensorId, ImageId("Photo.jpg")))
+
+  val nonEmptyRoom = NonEmptyRoom(SensorImage(room.sensorId, ImageId("Photo.jpg")), AlarmType.Info, List(person))
 }
 
 class RoomSpec extends FunSuite {
@@ -34,11 +37,11 @@ class RoomSpec extends FunSuite {
 
 class PersonSpec extends FunSuite {
   import Aggregates._
-  
+
   test("the id of the person should be 1") {
     assert(person.personId.value == 1)
   }
-  
+
   test("the name of the person should be Mrs Neeltje") {
     assert(person.name.value == "Mrs Neeltje")
   }
@@ -58,4 +61,34 @@ class ApartmentSpec extends FunSuite {
   test("the name of the apartment should be: Vesuvius") {
     assert(apartment.name.value == "Vesuvius")
   }
+
 }
+
+class EmptyRoomSpec extends FunSuite {
+    import Aggregates._
+
+    test("the sensorId should be 10100") {
+      assert(emptyRoom.sensorImage.sensorId.value == 10100)
+    }
+
+  }
+
+class NonEmptyRoomSpec extends FunSuite {
+    import Aggregates._
+    
+    test("the sensorId should be 10100") {
+      assert(nonEmptyRoom.sensorImage.sensorId.value == 10100)
+    }
+    
+    test("the alarmType should be Info") {
+      assert(nonEmptyRoom.alarmType == AlarmType.Info)
+    }
+    
+    test("the personId should be 1") {
+      assert(nonEmptyRoom.persons(0).personId.value == 1)
+    }
+    
+    test("the personName should be Mrs Neeltje") {
+      assert(nonEmptyRoom.persons(0).name.value == "Mrs Neeltje")
+    }
+  }
